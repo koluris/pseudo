b05b3b3e958f1a662c0c68781cc68b6c85b3db80
Dennis Koluris <dkoluris@outlook.com>
2018-03-21 14:30:54 +0200

    Godspeed.

---
+++ Platforms/macOS/Source/Main.m
@@ -0,0 +1,16 @@
+#import "Main.h"
+//#import "../../../Source/R3000A.h"
+
+@implementation Main
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+}
+
+@end
+
+int main() {
+    return NSApplicationMain(0, nil);
+}
---
+++ Source/R3000A.cpp
@@ -0,0 +1,1 @@
+#include "R3000A.h"
