15222096a87829858415acfcbeecf7cdf15fced0
Dennis Koluris <dkoluris@outlook.com>
2018-03-23 21:15:20 +0200

    Decode instructions.

--- Source/R3000A.cpp
+++ Source/R3000A.cpp
@@ -3,15 +3,33 @@
 
 CstrMips cpu;
 
+#define op\
+    ((code >> 26) & 63)
+
+// LUI
+//
+// 32 | 16 |  8 |  4 |  2 |  1 |
+// ----------------------------- = 15
+//  0 |  0 |  1 |  1 |  1 |  1 |
+
 void CstrMips::reset() {
     memset(base, 0, sizeof(base));
     pc = 0xbfc00000;
+    
     step();
 }
 
 void CstrMips::step() {
     uw code = mem.read32(pc);
+    printf("PC: 0x%08x, Code: 0x%08x\n", pc, code);
+    printf("%d\n", op);
+    pc += 4;
+    base[0] = 0;
     
-    printf("Code -> 0x%08x\n", code);
+    switch(op) {
+        case 15: // LUI
+            return;
+    }
+    
     exit(0);
 }
