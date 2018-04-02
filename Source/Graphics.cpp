3c335191939b948757a09fd6d301a6fd24765a6c
Dennis Koluris <dkoluris@outlook.com>
2018-04-02 14:23:46 +0300

    GPU handle.

--- Source/Graphics.cpp
+++ Source/Graphics.cpp
@@ -10,13 +10,40 @@ CstrGraphics vs;
 void CstrGraphics::reset() {
     ret.data   = 0x400;
     ret.status = 0x14802000;
+    
+    // Command buffer
+    memset(pipe.data, 0, sizeof(pipe.data));
+    pipe.prim = 0;
+    pipe.size = 0;
+    pipe.row  = 0;
 }
 
 void CstrGraphics::dataMemWrite(uw *ptr, sw size) {
     ret.data = *ptr;
     ptr++;
     
-    printx("GPU Data Write -> 0x%x", (GPU_COMMAND(ret.data)));
+    if (!pipe.size) {
+        ub prim  = GPU_COMMAND(ret.data);
+        ub count = pSize[prim];
+        
+        if (count) {
+            pipe.data[0] = ret.data;
+            pipe.prim = prim;
+            pipe.size = count;
+            pipe.row  = 1;
+        }
+    }
+    else {
+        pipe.data[pipe.row] = ret.data;
+        pipe.row++;
+    }
+    
+    if (pipe.size == pipe.row) {
+        pipe.size = 0;
+        pipe.row  = 0;
+        
+        printx("GPU Data Write -> 0x%x", (GPU_COMMAND(ret.data)));
+    }
 }
 
 void CstrGraphics::dataWrite(uw data) {
