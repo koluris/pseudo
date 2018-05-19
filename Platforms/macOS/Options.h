7df2fad701e20d5b67f21755d9651168ae9c8aa1
Dennis Koluris <dkoluris@outlook.com>
2018-05-19 21:44:29 +0300

    Option defaults.

--- Platforms/macOS/Options.h
+++ Platforms/macOS/Options.h
@@ -1,3 +1,12 @@
 @interface Options : NSPanel
 
+@prop (weak) IBOutlet NSPopUpButton *cpuMode;
+@prop (weak) IBOutlet NSButton *skipBootScreen;
+@prop (weak) IBOutlet NSPopUpButton *windowResolution;
+@prop (weak) IBOutlet NSButton *smoothTextures;
+@prop (weak) IBOutlet NSPopUpButton *fpsLimit;
+@prop (weak) IBOutlet NSButton *audioStereo;
+@prop (weak) IBOutlet NSButton *audioMono;
+@prop (weak) IBOutlet NSButton *audioOff;
+
 @end
--- Platforms/macOS/Options.mm
+++ Platforms/macOS/Options.mm
@@ -3,18 +3,21 @@
 
 @implementation Options
 
-//- (id)init {
-//    self = [super init];
-//
-//    if (self) {
-//        [self setFrame:CGRectMake(0, 0, 640, 480) display:NO];
-//        printf("debug A\n");
-//    }
-//    return self;
-//}
-
 - (void)awakeFromNib {
+    //NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    //[defaults setInteger:9001 forKey:@"HighScore"];
+    //[defaults synchronize];
+    
     printf("awakeFromNib\n");
+    
+    [self.cpuMode selectItemWithTitle:@"Interpreter"];
+    [self.skipBootScreen setState:NSOffState];
+    [self.windowResolution selectItemWithTitle:@"Native"];
+    [self.smoothTextures setState:NSOnState];
+    [self.fpsLimit selectItemWithTitle:@"60 (PAL)"];
+    [self.audioStereo setState:NSOnState];
+    [self.audioMono setState:NSOffState];
+    [self.audioOff setState:NSOffState];
 }
 
 @end
