38fad387d375ba39fb365c89fff22c214aae5e15
Dennis Koluris <dkoluris@outlook.com>
2018-06-29 16:00:19 +0300

    Minor.

--- Source/Graphics.cc
+++ Source/Graphics.cc
@@ -199,7 +199,10 @@ VRAM_END:
         modeDMA = GPU_DMA_NONE;
     }
     
-    return (count + (count % 2 ? 1 : 0)) >> 1;
+    if (count % 2) {
+        count++;
+    }
+    return count >> 1;
 }
 
 void CstrGraphics::dataWrite(uw *ptr, sw size) {
