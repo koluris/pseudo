51e7346fe60fc8c07f06e125286fcd5aefe4e2d5
Dennis Koluris <denniskoluris@rezolve.com>
2020-10-14 17:17:26 +0300

    Comments

--- Source/Sound.cc
+++ Source/Sound.cc
@@ -4,12 +4,20 @@
 
 CstrAudio audio;
 
+#define spuChannel(addr) \
+    (addr >> 4) & 0x1f
+
 void CstrAudio::reset() {
+    memset(&spuMem, 0, sizeof(spuMem));
     spuAddr = 0xffffffff;
     
     for (auto &item : spuVoices) {
         item = { 0 };
     }
+}
+
+sh CstrAudio::setVolume(sh data) {
+    return ((data & 0x7fff) ^ 0x4000) - 0x4000;
 }
 
 void CstrAudio::voiceOn(uw data) {
@@ -24,15 +32,11 @@ void CstrAudio::voiceOn(uw data) {
 void CstrAudio::voiceOff(uw data) {
 }
 
-int CstrAudio::setVolume(sh data) {
-    return ((data & 0x7fff) ^ 0x4000) - 0x4000;
-}
-
 void CstrAudio::write(uw addr, uh data) {
     switch(LOW_BITS(addr)) {
         case 0x1c00 ... 0x1d7e: // Channels
             {
-                int ch = (addr >> 4) & 0x1f;
+                int ch = spuChannel(addr);
 
                 switch(addr & 0xf) {
                     case 0x0: // Volume L
@@ -126,7 +130,7 @@ uh CstrAudio::read(uw addr) {
     switch(LOW_BITS(addr)) {
         case 0x1c00 ... 0x1d7e: // Channels
             {
-                int ch = (addr >> 4) & 0x1f;
+                int ch = spuChannel(addr);
 
                 switch(addr & 0xf) {
                     case 0xc: // Hack
@@ -326,29 +330,6 @@ void CstrAudio::executeDMA(CstrBus::castDMA *dma) {
     printx("/// PSeudo SPU DMA: 0x%x", dma->chcr);
 }
 
-//void CstrAudio::reset() {
-//    // Mem
-//    memset(&spuMem, 0, sizeof(spuMem));
-//    spuAddr = 0xffffffff;
-//
-//    // Channels reset
-//    for (auto &item : spuVoices) {
-//        item = { 0 };
-//    }
-//}
-//
-//sh CstrAudio::setVolume(sh data) { // Sweep mode
-//    return ((data & 0x7fff) ^ 0x4000) - 0x4000;
-//}
-//
-//#define audioSet(a, b) \
-//    rest = (*p & a) << b; \
-//    if (rest & 0x8000) rest |= 0xffff0000; \
-//    rest = (rest >> shift) + ((s[0] * f[predict][0] + s[1] * f[predict][1] + 32) >> 6); \
-//    s[1] = s[0]; \
-//    s[0] = MIN(MAX(rest, SHRT_MIN), SHRT_MAX); \
-//    chn.bfr[chn.size++] = s[0]
-//
 //void CstrAudio::voiceOn(uw data) {
 //    for (int n = 0; n < SPU_CHANNELS; n++) {
 //        if (data & (1 << n)) {
@@ -390,22 +371,6 @@ void CstrAudio::executeDMA(CstrBus::castDMA *dma) {
 //            }
 //        }
 //        NEXT_CHANNEL: ;
-//    }
-//}
-//
-//void CstrAudio::voiceOff(uw data) {
-//    for (int n = 0; n < SPU_CHANNELS; n++) {
-//        if (data & (1 << n)) {
-//        }
-//    }
-//}
-//
-//void CstrAudio::stream() {
-//    ALint state;
-//    alGetSourcei(source, AL_SOURCE_STATE, &state);
-//
-//    if (state != AL_PLAYING) {
-//        alSourceStream(source);
 //    }
 //}
 //
@@ -453,31 +418,8 @@ void CstrAudio::executeDMA(CstrBus::castDMA *dma) {
 //            sbuf[i + 0] = MIN(MAX(+(temp[i + 0] / 2), SHRT_MIN), SHRT_MAX);
 //            sbuf[i + 1] = MIN(MAX(-(temp[i + 1] / 2), SHRT_MIN), SHRT_MAX);
 //        }
-//
-//        // OpenAL
-//        ALint processed;
-//        alGetSourcei(source, AL_BUFFERS_PROCESSED, &processed);
-//
-//        if (processed >= SPU_ALC_BUF_AMOUNT) {
-//            // We have to free buffers
-//            printf("/// PSeudo Inadequent ALC buffer size -> %d\n", processed);
-//        }
-//
-//        while(--processed < 0) {
-//            stream();
-//            alGetSourcei(source, AL_BUFFERS_PROCESSED, &processed);
-//        }
-//
-//        ALuint buffer;
-//        alSourceUnqueueBuffers(source, 1, &buffer);
-//        alBufferData(buffer, AL_FORMAT_STEREO16, sbuf, SPU_SAMPLE_SIZE * 2, SPU_SAMPLE_RATE);
-//        alSourceQueueBuffers(source, 1, &buffer);
-//        stream();
 //    }
 //}
-//
-//#define spuChannel(addr) \
-//    (addr >> 4) & 0x1f
 //
 //void CstrAudio::write(uw addr, uh data) {
 //    switch(LOW_BITS(addr)) {
@@ -570,90 +512,4 @@ void CstrAudio::executeDMA(CstrBus::castDMA *dma) {
 //    }
 //
 //    printx("/// PSeudo SPU write: 0x%x <- 0x%x", addr, data);
-//}
-//
-//uh CstrAudio::read(uw addr) {
-//    switch(LOW_BITS(addr)) {
-//        case 0x1c00 ... 0x1d7e: // Channels
-//            {
-//                ub n = spuChannel(addr);
-//
-//                switch(addr & 0xf) {
-//                    case 0xc: // Hack
-//                        if (spuVoices[n].size) {
-//                            return 1;
-//                        }
-//                        return 0;
-//
-//                    /* unused */
-//                    case 0x0:
-//                    case 0x2:
-//                    case 0x4:
-//                    case 0x6:
-//                    case 0x8:
-//                    case 0xa:
-//                    case 0xe: // madman
-//                        return accessMem(mem.hwr, uh);
-//                }
-//
-//                printx("/// PSeudo SPU read phase: 0x%x", (addr & 0xf));
-//                return 0;
-//            }
-//
-//        case 0x1da6: // Transfer Address
-//            return spuAddr >> 3;
-//
-//        /* unused */
-//        case 0x1d88: // Sound On 1
-//        case 0x1d8a: // Sound On 2
-//        case 0x1d8c: // Sound Off 1
-//        case 0x1d8e: // Sound Off 2
-//        case 0x1d94: // Noise Mode On 1
-//        case 0x1d96: // Noise Mode On 2
-//        case 0x1d98: // Reverb Mode On 1
-//        case 0x1d9a: // Reverb Mode On 2
-//        case 0x1d9c: // Voice Status 0 - 15
-//        case 0x1daa: // Control
-//        case 0x1dac: // ?
-//        case 0x1dae: // Status
-//        case 0x1db8:
-//        case 0x1dba:
-//        case 0x1e00:
-//        case 0x1e02:
-//        case 0x1e04:
-//        case 0x1e06:
-//        case 0x1e08:
-//        case 0x1e0a:
-//        case 0x1e0c:
-//        case 0x1e0e:
-//            return accessMem(mem.hwr, uh);
-//    }
-//
-//    printx("/// PSeudo SPU read: 0x%x", addr);
-//    return 0;
-//}
-//
-//void CstrAudio::executeDMA(CstrBus::castDMA *dma) {
-//    uh *p   = (uh *)&mem.ram.ptr[dma->madr & (mem.ram.size - 1)];
-//    uw size = (dma->bcr >> 16) * (dma->bcr & 0xffff) * 2;
-//
-//    switch(dma->chcr) {
-//        case 0x01000201: // Write
-//            for (uw i = 0; i < size; i++) {
-//                spuMem[spuAddr >> 1] = *p++;
-//                spuAddr += 2;
-//                spuAddr &= 0x7ffff;
-//            }
-//            return;
-//
-//        case 0x01000200: // Read
-//            for (uw i = 0; i < size; i++) {
-//                *p++ = spuMem[spuAddr >> 1];
-//                spuAddr += 2;
-//                spuAddr &= 0x7ffff;
-//            }
-//            return;
-//    }
-//
-//    printx("/// PSeudo SPU DMA: 0x%x", dma->chcr);
 //}
--- Source/Sound.h
+++ Source/Sound.h
@@ -37,9 +37,9 @@ class CstrAudio {
         
     } spuVoices[SPU_MAX_CHAN];
     
-    void voiceOn(uw);
+    sh setVolume(sh);
+    void voiceOn (uw);
     void voiceOff(uw);
-    int setVolume(sh);
     void output();
     void stream();
     
@@ -85,94 +85,17 @@ public:
         alcCloseDevice(device);
     }
     
-    void init();
     void reset();
-    void write(uw, uh);
-    uh read(uw);
-    void executeDMA(CstrBus::castDMA *);
     void decodeStream();
+    
+    // Store
+    void write(uw, uh);
+    
+    // Load
+    uh read(uw);
+    
+    // DMA
+    void executeDMA(CstrBus::castDMA *);
 };
 
 extern CstrAudio audio;
-
-//class CstrAudio {
-//    enum {
-//        SPU_ALC_BUF_AMOUNT   = 16,
-//        SPU_CHANNELS         = 24,
-//        SPU_CHANNEL_BUF_SIZE = 11 * (USHRT_MAX + 1), // 2 would cover it, but... hs-001
-//        SPU_MAX_VOLUME       = 0x3fff,
-//        SPU_SAMPLE_RATE      = 44100,
-//        SPU_SAMPLE_SIZE      = 512,
-//    };
-//
-//    const sh f[5][2] = {
-//        { 0, 0 }, { 60, 0 }, { 115, -52 }, { 98, -55 }, { 122, -60 }
-//    };
-//
-//    struct voice {
-//        sh bfr[SPU_CHANNEL_BUF_SIZE];
-//        sw count, freq, pos, raddr, saddr, size;
-//        sh volumeL, volumeR;
-//    } spuVoices[SPU_CHANNELS];
-//
-//    uh spuMem[1024 * 256];
-//    uw spuAddr;
-//    sh sbuf[SPU_SAMPLE_SIZE];
-//
-//    sh setVolume(sh);
-//    void voiceOn (uw);
-//    void voiceOff(uw);
-//    void stream();
-//
-//    // OpenAL
-//    ALCdevice *device;
-//    ALCcontext *ctx;
-//    ALuint source;
-//    ALuint bfr[SPU_ALC_BUF_AMOUNT];
-//
-//public:
-//    CstrAudio() {
-//        // OpenAL
-//        device = alcOpenDevice(NULL);
-//
-//        if (!device) {
-//            printf("ALC Device error\n");
-//        }
-//
-//        ctx = alcCreateContext(device, NULL);
-//        alcMakeContextCurrent(ctx);
-//
-//        if (!ctx) {
-//            printf("ALC Context error\n");
-//        }
-//
-//        alGenSources(1, &source);
-//        alGenBuffers(SPU_ALC_BUF_AMOUNT, bfr);
-//
-//        for (auto &item : bfr) {
-//            alBufferData(item, AL_FORMAT_STEREO16, sbuf, SPU_SAMPLE_SIZE * 2, SPU_SAMPLE_RATE);
-//        }
-//
-//        alSourceQueueBuffers(source, SPU_ALC_BUF_AMOUNT, bfr);
-//    }
-//
-//    ~CstrAudio() {
-//        alDeleteSources(1, &source);
-//        alDeleteBuffers(SPU_ALC_BUF_AMOUNT, bfr);
-//        ALCdevice *device = alcGetContextsDevice(ctx);
-//        alcMakeContextCurrent(NULL);
-//        alcCloseDevice(device);
-//    }
-//
-//    void reset();
-//    void decodeStream();
-//
-//    // Store
-//    void write(uw, uh);
-//
-//    // Load
-//    uh read(uw);
-//
-//    // DMA
-//    void executeDMA(CstrBus::castDMA *);
-//};
